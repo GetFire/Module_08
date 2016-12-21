bcc5ebf4076544648c1242a7c29e45bfc4eb0620
Denis Azhymov <azhimov93@gmail.com>
2016-12-21 16:58:00 +0200

    init

--- src/HomeWork/AbstractDAO.java
+++ src/HomeWork/AbstractDAO.java
@@ -1,8 +1,17 @@
 package HomeWork;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface AbstractDAO<T> {
+//    List<T> list;
     T save (T aSave);
     void delete(T aDelete);
+    void deleteAll(List<T> list);
+    void saveAll (List<T>list);
+
+    void deleteById(long id);
+    T get(long id);
 
 
 }
